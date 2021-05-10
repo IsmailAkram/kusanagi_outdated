c3b38684a46f84b12414faeb95630f89b3db76ad
IsmailAkram <IzzyAkram@outlook.com>
2021-05-10 18:03:55 -0400

    Moved precedences to SyntaxFacts

--- kc/Code_Analysis/Parser.cs
+++ kc/Code_Analysis/Parser.cs
@@ -84,7 +84,7 @@ namespace Kusanagi.Code_Analysis
 
             while (true)
             {
-                var precedence = GetBinaryOperatorPrecedence(Current.Kind);
+                var precedence = Current.Kind.GetBinaryOperatorPrecedence();
                 if (precedence == 0 || precedence <= parentPrecedence)
                     break;
 
@@ -94,23 +94,6 @@ namespace Kusanagi.Code_Analysis
             }
 
             return left;
-        }
-
-        private static int GetBinaryOperatorPrecedence(SyntaxKind kind)
-        {
-            switch(kind)
-            {
-                case SyntaxKind.StarToken:
-                case SyntaxKind.SlashToken:
-                    return 2;
-
-                case SyntaxKind.PlusToken:
-                case SyntaxKind.MinusToken:
-                    return 1;
-
-                default:
-                    return 0;
-            }
         }
 
         private ExpressionSyntax ParsePrimaryExpression()
